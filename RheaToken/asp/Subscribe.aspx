c5f1c517563a98e070c92c56a53bbe8d69fcae50
vantonov <vess28@yahoo.com>
2017-09-03 13:07:13 +0300

    Implemented initial subscribe skeleton

---
+++ RheaToken/asp/Subscribe.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Subscribe.aspx.cs" Inherits="asp_Subscribe" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+    <title>Subscription</title>
+    <style type="text/css">
+        .style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <p>Please enter your e-mail and we will send you news and updates concerning Rhea!</p>
+    <form id="form2" runat="server">
+        <asp:TextBox ID="textEmail" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSubscribe" runat="server" Text="Subscribe"
+            OnClick="BtnSubmit" />
+    </form>
+</body>
+</html>
---
+++ RheaToken/asp/Subscribe.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class asp_Subscribe : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public string GetConnectionString()
+    {
+        //sets the connection string from your web config file "ConnString" is the name of your Connection String
+        return System.Configuration.ConfigurationManager.ConnectionStrings["MyConsString"].ConnectionString;
+    }
+
+    private void ExecuteInsert(string email)
+    {
+        SqlConnection conn = new SqlConnection(GetConnectionString());
+        string sql = "INSERT INTO RheaSubscriptions.dbo.Subscriptions VALUES "
+                    + " (@email)";
+
+        try
+        {
+            /*
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlParameter[] param = new SqlParameter[1];
+            //param[0] = new SqlParameter("@id", SqlDbType.Int, 20);
+            param[0] = new SqlParameter("@email", SqlDbType.VarChar, 255);
+
+            param[0].Value = email;
+
+            cmd.Parameters.Add(param[0]);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            */
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlParameter param = new SqlParameter();
+            //param[0] = new SqlParameter("@id", SqlDbType.Int, 20);
+            param = new SqlParameter("@email", SqlDbType.VarChar, 255);
+
+            param.Value = email;
+
+            cmd.Parameters.Add(param);
+
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+        }
+        catch (SqlException ex)
+        {
+            string msg = "Insert Error:";
+            msg += ex.Message;
+            throw new Exception(msg);
+        }
+        catch (Exception ex)
+        {
+            Response.Write("Service is unavailable please try again later!");
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+    protected void BtnSubmit(object sender, EventArgs e)
+    {
+        string currentEmail;
+
+        try
+        {
+            currentEmail = textEmail.Text.Trim();
+        }
+        catch (Exception)
+        {
+            currentEmail = string.Empty;
+        }
+
+        if (string.IsNullOrEmpty(currentEmail))
+        {
+            Response.Write("Please enter your e-mail.");
+            textEmail.Focus();
+            return;
+        }
+
+        if (!currentEmail.Contains("@"))
+        {
+            Response.Write("Your e-mail must contain an '@' symbol.");
+            textEmail.Focus();
+            return;
+        }
+
+        //call the method to execute insert to the database
+        ExecuteInsert(textEmail.Text);
+        Response.Write("Record was successfully added!");
+        //ClearControls(Page);
+
+    }
+}
